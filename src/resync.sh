1acb9e85b42eaf0475df0a0a976865f2ed1f222a
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-05-26 21:20:40 +0200

    Handle CTRL+C correctly

--- src/resync.sh
+++ src/resync.sh
@@ -14,6 +14,8 @@ if [[ ! -d "$1" ]]; then
     exit 1
 fi
 
+trap "exit" INT
+
 while inotifywait -r -e modify,create,delete,move "$1"; do
     for remote in $*; do
         rsync -avz --progress "$1" $remote:
