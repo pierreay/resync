af2356ef22ad9983a6e11537405ffad93abb4096
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-06-27 22:54:37 +0200

    [src/resync] fix date logging

--- src/resync.sh
+++ src/resync.sh
@@ -78,7 +78,7 @@ done
 
 while inotifywait -r -e modify,create,delete,move "$source"; do
     for remote in $*; do
-        log_info $(date --rfc-3339=seconds)
+        log_info "$(date --rfc-3339=seconds)"
         rsync -avz --progress "$source" $remote:
     done
 done
