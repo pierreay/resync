024bd3c34902f25e482907020fb0d26ae3731354
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-06-27 22:52:24 +0200

    [readme;src/*] make resync one-file with logging

--- src/resync.sh
+++ src/resync.sh
@@ -1,6 +1,56 @@
 #!/bin/bash
 
-source log.sh
+# * log_color.sh
+# Cloned from:
+# https://github.com/pierreay/examples/blob/main/bash/log_color.sh
+
+# Colors definition.
+black="\e[30;1m"
+red="\e[31;1m"
+green="\e[32;1m"
+yellow="\e[33;1m"
+blue="\e[34;1m"
+magenta="\e[35;1m"
+cyan="\e[36;1m"
+white="\e[37;1m"
+
+# Reset text attributes to normal without clearing screen.
+function creset() {
+    tput sgr0
+}
+
+# Color-echo.
+# Argument $1 = message
+# Argument $2 = color
+function cecho() {
+    message="$1"          # Defaults to default message.
+    color="${2:-$green}"  # Defaults to green, if not specified.
+    echo -n -e "$color"
+    echo -n "$message"
+    creset
+}
+
+function log_error() {
+    cecho "[ERROR] " $red
+    echo $1
+}
+
+function log_warn() {
+    cecho "[WARN]  " $yellow
+    echo $1
+}
+
+function log_debug() {
+    cecho "[DEBUG] " $white
+    echo $1
+}
+
+function log_info() {
+    cecho "[INFO]  " $green
+    echo $1
+}
+
+# * resync.sh
 
 if [[ $# < 2 ]]; then
     cat << EOF
